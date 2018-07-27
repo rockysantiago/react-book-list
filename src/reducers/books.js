4cf4d7aa2b26c0719c28416734a622c1061b1ef2
rockysantiago <raphael.santiago@icloud.com>
2018-07-27 16:57:03 +0800

    add number of pages

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,9 +1,12 @@
 export default () => {
   return [
-    { title: 'Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future' },
-    { title: 'Steve Jobs' },
-    { title: 'How to Win Friends & Influence People' },
-    { title: 'Harry Potter And The Chamber Of Secrets' },
-    { title: 'The Lord of the Rings: The Return of the King' }
+    {
+      title: 'Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future',
+      pages: 100
+    },
+    { title: 'Steve Jobs', pages: 200 },
+    { title: 'How to Win Friends & Influence People', pages: 300 },
+    { title: 'Harry Potter And The Chamber Of Secrets', pages: 400 },
+    { title: 'The Lord of the Rings: The Return of the King', pages: 500 }
   ];
 };
