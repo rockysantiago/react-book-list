f16d6f394b1a982b4b1efbc0f51aff53e8e5f65b
rockysantiago <raphael.santiago@icloud.com>
2018-07-27 15:53:17 +0800

    add books mock

---
+++ src/reducers/books.js
@@ -0,0 +1,9 @@
+export default () => {
+  return [
+    { title: 'Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future' },
+    { title: 'Steve Jobs' },
+    { title: 'How to Win Friends & Influence People' },
+    { title: 'Harry Potter And The Chamber Of Secrets' },
+    { title: 'The Lord of the Rings: The Return of the King' }
+  ];
+};
